e53539e2722b193cd730f2bae7693dc2d6f3c350
oliver <mail@oliver-kent.co.uk>
2016-05-06 00:14:44 +0100

    Google Auth Working!
    Good, reliable Google auth code working. The client side html/jscript is a mess, and needs to be sorted. Server side code needs to be moved from experiments and implemented in the manf server.

--- experiments/custom_login.py
+++ experiments/custom_login.py
@@ -5,12 +5,12 @@ import json
 
 from flask import Flask
 from flask import make_response
-from flask import render_template
+from flask import render_template, redirect
 from flask import request
 from flask import send_file
 from flask import session
 
-from google_auth import GoogleAuth, LoginFailure
+from google_auth import GoogleAuth, LoginFailure, LogoutFailure
 import httplib2
 
 APPLICATION_NAME = 'Google+ Python Quickstart'
@@ -18,20 +18,40 @@ APPLICATION_NAME = 'Google+ Python Quickstart'
 
 app = Flask(__name__)
 app.secret_key = 'abcdefghijklmnopqrstuvwxyz111111'
+app.google_auth = GoogleAuth("client_secrets.json")
 
+def BadJsonResponse(data, status_code=401):
+    response = make_response(json.dumps(data), status_code)
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+def GoodJsonResponse(data, status_code=200):
+    response = make_response(json.dumps(data), status_code)
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/', methods=['GET'])
 def index():
 
-    app.google_auth.start_login(session)
+    app.google_auth.start_login()
 
-    response = make_response(
-        render_template('index.html',
-                        CLIENT_ID=app.google_auth.client_id,
-                        STATE=session['state'],
-                        APPLICATION_NAME="manf-demo1"))
-    response.headers['Content-Type'] = 'text/html'
-    return response
+
+    return render_template( 'index.html',
+                            CLIENT_ID           = app.google_auth.client_id,
+                            STATE               = session['auth_state'],
+                            APPLICATION_NAME    = "manf-demo1" )
+
+@app.route("/secret")
+def secret():
+
+    if app.google_auth.is_authenticated():
+
+        return "You are logged in!"
+
+    else:
+        return redirect('/')
+
+
 
 @app.route('/signin_button.png', methods=['GET'])
 def signin_button():
@@ -40,101 +60,40 @@ def signin_button():
 
 @app.route('/connect', methods=['POST'])
 def connect():
-    """Exchange the one-time authorization code for a token and
-    store the token in the session."""
-    # Ensure that the request is not a forgery and that the user sending
-    # this connect request is the expected user.
-    if request.args.get('state', '') != session['state']:
-        response = make_response(json.dumps('Invalid state parameter.'), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-    # Normally, the state is a one-time token; however, in this example,
-    # we want the user to be able to connect and disconnect
-    # without reloading the page.  Thus, for demonstration, we don't
-    # implement this best practice.
-    # del session['state']
 
     try:
-        app.google_auth.process_login(session, request.data)
+        app.google_auth.process_login(request)
 
     except LoginFailure as failure:
-
-        response = make_response( json.dumps(failure.reason), failure.code )
-        response.headers['Content-Type'] = 'application/json'
-        return response
+        return BadJsonResponse(failure.reason)
 
     else:
 
         if session.get('relogin') is True:
-            response = make_response(json.dumps('Current user is already connected.'),
-                                 200)
-            response.headers['Content-Type'] = 'application/json'
-            return response
+            return GoodJsonResponse('Current user is already connected.')
 
         else:
-            response = make_response(json.dumps('Successfully connected user.'), 200)
-            response.headers['Content-Type'] = 'application/json'
-            return response
-
+            return GoodJsonResponse('Successfully connected user.')
 
 @app.route('/disconnect', methods=['POST'])
 def disconnect():
     """Revoke current user's token and reset their session."""
-
-    # Only disconnect a connected user.
-    credentials = session.get('credentials')
-    if credentials is None:
-        response = make_response(json.dumps('Current user not connected.'), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-
-    # Execute HTTP GET request to revoke current token.
-    access_token = credentials.access_token
-    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
-    h = httplib2.Http()
-    result = h.request(url, 'GET')[0]
-
-    if result['status'] == '200':
-        # Reset the user's session.
-        del session['credentials']
-        response = make_response(json.dumps('Successfully disconnected.'), 200)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-    else:
-        # For whatever reason, the given token was invalid.
-        response = make_response(
-            json.dumps('Failed to revoke token for given user.', 400))
-        response.headers['Content-Type'] = 'application/json'
-        return response
-
-
-@app.route('/people', methods=['GET'])
-def people():
-    """Get list of people user has shared with this app."""
-    credentials = session.get('credentials')
-    # Only fetch a list of people for connected users.
-    if credentials is None:
-        response = make_response(json.dumps('Current user not connected.'), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
     try:
-        # Create a new authorized API client.
-        http = httplib2.Http()
-        http = credentials.authorize(http)
-        # Get a list of people that this user has shared with this app.
-        google_request = SERVICE.people().list(userId='me', collection='visible')
-        result = google_request.execute(http=http)
+        app.google_auth.revoke()
 
-        response = make_response(json.dumps(result), 200)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-    except AccessTokenRefreshError:
-        response = make_response(json.dumps('Failed to refresh access token.'), 500)
-        response.headers['Content-Type'] = 'application/json'
-        return response
+    # Catch LogoutFailure error, and return a bad response with the failure reason
+    except LogoutFailure as failure:
+        return BadJsonResponse(failure.reason)
+
+    # Re-raise any other exceptions
+    except:
+        raise
+
+    # Log out succeeded, return a good response
+    else:
+        return GoodJsonResponse("Successfully disconnected.")
 
 
 if __name__ == '__main__':
-    app.google_auth = GoogleAuth("client_secrets.json")
     app.debug = True
     app.run(host="localhost", port=4567)
--- experiments/google_auth.py
+++ experiments/google_auth.py
@@ -1,54 +1,161 @@
 
 # Library imports
 import json, random, string
-from apiclient.discovery import build
 from oauth2client.client import AccessTokenRefreshError
 from oauth2client.client import flow_from_clientsecrets
-from oauth2client.client import FlowExchangeError
+from oauth2client.client import FlowExchangeError, Credentials
+import requests
+import httplib2
+from flask import session
 
-class LoginFailure(Exception):
+
+
+class _AuthFailure(Exception):
     def __init__(self, code=None, reason=None):
         self.reason = reason
-        self.code   = code
+        self.code = code
+        session.pop('auth_state', None)
+        session.pop('auth_cred_json', None)
+        session.pop('auth_gplus_id', None)
+        session.pop('auth_relogin', None)
+
+class LoginFailure(_AuthFailure):
+    pass
+
+class LogoutFailure(_AuthFailure):
+    pass
+
+class NotLoggedInError(Exception):
+    pass
+
+class BadCredentialsError(Exception):
+    pass
+
 
 class GoogleAuth(object):
+
+    class Decorator(object):
+        def __init__(self, f):
+            pass
+
+        def __call__(self, *args, **kwargs):
+            print "this runs before the func"
+
 
     def __init__(self, client_secrets_json):
 
         # Init class vars
         self._client_secrets_json = client_secrets_json
         self.client_id = json.loads(open(client_secrets_json, 'r').read())['web']['client_id']
-        self._service   = build('plus', 'v1')
 
-    def start_login(self, session_dict):
+    def start_login(self):
         # Create random state variable to prevent request forgery (server will return it)
-        session_dict['state'] = self._generate_random_state_key()
+        session['auth_state'] = self._generate_random_state_key()
 
-    def process_login(self, session_dict, post_data, scope=None):
+    def process_login(self, response, scope=''):
 
         try:
             # Upgrade the authorization code into a credentials object
-            oauth_flow = flow_from_clientsecrets(self._client_secrets_json, scope='')
+            oauth_flow = flow_from_clientsecrets(self._client_secrets_json, scope=scope)
             oauth_flow.redirect_uri = 'postmessage'
-            credentials = oauth_flow.step2_exchange(post_data)
+            credentials = oauth_flow.step2_exchange(response.data)
 
-        # Catch flow exchange error and re-raise as generic login falure error
+        # Catch flow exchange error and re-raise as generic login failure error
         except FlowExchangeError:
-            raise LoginFailure(code=401, reason="Failed to upgrade the authorization code")
+            raise LoginFailure(reason="Failed to upgrade the authorization code")
 
         # Push any other exception further up
         except:
             raise
 
         else:
+            # Fetch local and remote state variables
+            remote_state    = response.args.get('state')
+            local_state     = session.get("auth_state", " ")
+
+            # If local and remote state variables don't match, raise a LoginFailure error
+            if local_state != remote_state:
+                raise LoginFailure( reason="Local state ({}) does not match remote state ({})".format(local_state,
+                                                                                                     remote_state) )
+
             # If the user was already logged in, set the relogin flag true
-            if session_dict.get('gplus_id') == credentials.id_token['sub']:
-                session_dict['relogin'] = True
+            if session.get('auth_gplus_id') == credentials.id_token['sub']:
+                session['auth_relogin'] = True
 
             # Grab login data and store in session dict
-            session_dict['gplus_id']    = credentials.id_token['sub']
-            #session_dict['credentials'] = credentials
+            session['auth_gplus_id']    = credentials.id_token['sub']
+            session['auth_cred_json']   = credentials.to_json()
+
+    def revoke(self):
+
+        # Try and get credentials from session
+        try:
+            credentials = self._get_credentials_from_session()
+
+        # Catch bad credentials error, and raise as a logout failure
+        except BadCredentialsError as error:
+            raise LogoutFailure(reason=error.message)
+
+        # Catch not logged in error, and raise as logout failure
+        except NotLoggedInError:
+            raise LogoutFailure(reason="Not logged in")
+
+        # Found good credentials
+        else:
+            # Send the request to revoke authentication
+            result = requests.get( credentials.revoke_uri, params={'token' : credentials.access_token} )
+
+            # Wipe session auth data
+            self._wipe_session_auth_data()
+
+            # If the request failed, then raise a LogoutFailure error
+            if not result:
+                raise LogoutFailure(code=result.status_code, reason=result.text)
+
+    def is_authenticated(self):
+
+        # Attempt to authorise a http request object
+        try:
+            http = httplib2.Http()
+            http = self._get_credentials_from_session().authorize(http)
+
+        # Catch any of the various login failures, wipe session auth data and return false
+        except (AccessTokenRefreshError, NotLoggedInError, BadCredentialsError):
+            self._wipe_session_auth_data()
+            return False
+
+        # Re-raise other exceptions
+        except:
+            raise
+
+        # Http request object authorise succeeded, user is still authenticated
+        else:
+            return True
 
 
     def _generate_random_state_key(self):
         return ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+
+    def _get_credentials_from_session(self):
+
+        # If we can't find the credentials json, then we are probably not logged in
+        if session.get('auth_cred_json') is None:
+            raise NotLoggedInError()
+
+        # Try and parse credentials json from session
+        try:
+            credentials = Credentials.new_from_json(session.get("auth_cred_json"))
+
+        # Catch value error (couldn't parse json), and raise a LogoutFailure error
+        except ValueError as error:
+            raise BadCredentialsError(message=error.message)
+
+        # Managed to fetch and parse the credentials object from json correctly, return the object
+        else:
+            return credentials
+
+    def _wipe_session_auth_data(self):
+        session.pop('auth_state', None)
+        session.pop('auth_cred_json', None)
+        session.pop('auth_gplus_id', None)
+        session.pop('auth_relogin', None)
